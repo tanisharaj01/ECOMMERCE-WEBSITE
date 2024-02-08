f52361a8d884fc4c58599e7e95cecf6a2411d5c6
tanisharaj01 <aestheticmelophile53@gmail.com>
2024-02-09 04:03:28 +0530

    Pre-finalCommit

--- ECOMMERCE/add.js
+++ ECOMMERCE/add.js
@@ -1,8 +1,4 @@
-// Get the button element by its ID
 const visitButton = document.getElementById('visitButton');
-
-// Add a click event listener to the button
 visitButton.addEventListener('click', function() {
-    // Redirect to the second HTML page
     window.location.href = 'content.html';
 });
